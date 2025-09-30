fb453b1cfdfab09aad57a1a01fed08958177458a
Semenov Maxim <zimylan.gmail.com>
2025-10-01 00:11:09 +0300

    setupDefaultPiecePosition add

--- src/main/Board.java
+++ src/main/Board.java
@@ -1,16 +1,21 @@
 package main;
 
+import main.pieces.Pawn;
+
 import java.util.HashMap;
 
 public class Board {
     HashMap<Coordinates, Piece> pieces = new HashMap<>();
 
-    public void setPieces(Piece piece, Coordinates coordinates) {
-        piece.coordinates = coordinates; // Обновление координат фигуры при расположении на доске
+    public void setPieces(Coordinates coordinates,Piece piece) {
         pieces.put(coordinates, piece);
+        piece.coordinates = coordinates; // Обновление координат фигуры при расположении на доске
     }
 
     public void setupDefaultPiecesPositions() {
-
+        for (File file : File.values()) {
+            setPieces(new Coordinates(file, 2), new Pawn(Color.WHITE, new Coordinates(file, 2)));
+            setPieces(new Coordinates(file, 7), new Pawn(Color.BLACK, new Coordinates(file, 7)));
+        }
     }
 }
