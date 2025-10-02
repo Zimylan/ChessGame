f373c7d6a6a8da4b55490bd73b6910a779b4a4a1
Semenov Maxim <zimylan.gmail.com>
2025-10-02 23:36:32 +0300

    Add BoardRenderer render method

--- src/main/BoardConsoleRenderer.java
+++ src/main/BoardConsoleRenderer.java
@@ -1,7 +1,13 @@
 package main;
 
 public class BoardConsoleRenderer {
-    public void render(Board board) {
 
+    public void render(Board board) {
+        for (int rank = 8; rank >= 1 ; rank--) {
+            for (File file : File.values()){
+                System.out.print(file + String.valueOf(rank));
+            }
+            System.out.print("\n");
+        }
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Board board = new Board();
         board.setupDefaultPiecesPositions();
+        BoardConsoleRenderer boardConsoleRenderer = new BoardConsoleRenderer();
+        boardConsoleRenderer.render(board);
     }
 }
